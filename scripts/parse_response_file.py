d238aaa3ee578a65745ff44b49fe16ca06cd047e
Matt Bellis <matthew.bellis@gmail.com>
2015-06-09 16:22:00 -0400

    Small fixes.

--- scripts/parse_response_file.py
+++ scripts/parse_response_file.py
@@ -14,18 +14,18 @@ def main():
     args = parser.parse_args()
 
     # Open the file and pull out the information.
-    questions,solutions,student_answers = None
-    if args.input_file_name is not None:
-        questions,solutions,student_answers = read_tab_file(args.input_file_name)
+    questions,solutions,student_answers = None,None,None
+    if args.infile_name is not None:
+        questions,solutions,student_answers = read_tab_file(args.infile_name)
 
     outfile_name = 'solutions.py'
-    outfile = open(outfile_name,'rb')
+    outfile = open(outfile_name,'w+')
 
     output = "solutions = [ \n"
 
     # For now, assume the solutions are the first one.
-    for solution in solutions[0]:
-        output += "\t\'%s\'\n" % (solution)
+    for solution in solutions:
+        output += "\t\"%s\"\n" % (solution)
 
     output += "] \n"
 
