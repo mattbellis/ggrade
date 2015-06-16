3640d71536a52f0675c18ac893f3d9815a47236b
Sara Mahar <se30maha@siena.edu>
2015-06-16 11:34:53 -0400

    Added latest...

--- scripts/grade_google_form.py
+++ scripts/grade_google_form.py
@@ -4,20 +4,42 @@ import getpass
 import sys
 import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
+import csv
 
-################################################################################
+###############################################################################
 # Read in the file with the student responses. 
 # (Response Form from Google Forms)
-################################################################################
-infilename = sys.argv[1]
+###############################################################################
+make_plots = False
+send_emails = False
 
+if len(sys.argv) >2:
+    print "yes"
+    if sys.argv[2]=="email":
+         send_emails = True
+    elif sys.argv[2] == "plots":
+         make_plots=True
+    else:
+         print "Invalid entry for 4th entry"
+    if len(sys.argv) > 3:
+        if sys.argv[3]=="email":
+            send_emails = True
+        elif sys.argv[3] == "plots":
+            make_plots=True
+        else:
+            print "Invalid entry for 5th entry"
+
+infilename = sys.argv[1]
 questions,solutions,student_responses=read_tab_file(infilename)
 
 
-################################################################################
+###############################################################################
 # Read in the solutions from a different file.
+# Should change solutions_filename with each quiz that has different solutions
+# and feeback feedback.
+###############################################################################
 #solutions_filename = sys.argv[1].strip('.py')
-solutions_filename = 'solutions'
+solutions_filename = 'solutions' 
 
 solutions_file = __import__(solutions_filename)
 
@@ -29,14 +51,19 @@ print solutions
 print feedback_for_everyone
 print feedback_for_wrong_answers
 
-################################################################################
-
-# We will need put in the password here. 
+###############################################################################
+# If "emails" and entered into the command line, it will prompt the user for 
+# their Google username and password.
+###############################################################################
 my_email_address = None
 password = None
 
-#my_email_address = getpass.getpass("Enter address from which to send email: ")
-#password = getpass.getpass()
+if send_emails:
+    my_email_address = getpass.getpass("Enter address from which to send email: ")
+    password = getpass.getpass()
+
+scores_file = csv.writer(open('student_scores.csv', "wb"),delimiter=',')
+scores_file.writerow(['Time -------------------------Student Email------------Student Score'])
 
 points_per_question=10
 
@@ -59,7 +86,7 @@ for i,student in enumerate(student_responses):
     total_possible=0 # Total possible points achievable.
 
     # Grab the student email and the timestamp of when they submitted their
-    # work.
+    # work- if username is automatically grabbed from Google Forms.
     student_email=student[0]
     #student_email="se30maha@siena.edu"
     time = student[1]
@@ -78,19 +105,21 @@ for i,student in enumerate(student_responses):
             assignment_summary[i][question_number]=0
 
         # Attach the feedback from that question to the total feedback we'll be 
-        # emailing the stduent.
+        # emailing the student.
         output += sub_output
 
-        # Ccalculate total number of points and points possibl.e
+        # Calculate total number of points and points possible.
         total += points_received 
         total_possible += points_possible 
-    student_scores.append((total/float(total_possible))*100) 
-    student_info[student_email]= ((total/float(total_possible))*100)
+    this_student_score = round((total/float(total_possible))*100,2)
+    student_scores.append(this_student_score) 
+    student_info[student_email]= (this_student_score)
 
     # Now we've looped over all the problems for that student.
     # Append a string which summarizes their performance. 
     output += "<center> <br> <br> <b> Grade: %6.3f out of %d ----- %4.2f </b> </center>" % (total,total_possible,100*(total/float(total_possible)))
-
+    
+    scores_file.writerow([time,'----------',student_email,'----------',this_student_score])
 
     ###########################################################################
     # Email the student the feedback.
@@ -98,63 +127,66 @@ for i,student in enumerate(student_responses):
     if password is not None:
          email_grade_summaries(student_email,my_email_address,'Brownie and cookie physics test',output,password,isHTML=True)
 
+
 ###############################################################################
 # Plots each student's score on one figure. It then loops through each students
 # scores and plots the current student's score. So the number of figures equals 
 # the number of students. Each student has a different graph depending on what 
 # their score is. Also, sorts the scores.
 ###############################################################################
-student_scores=sorted(student_scores)
 
-average=sum(student_scores)/float(len(student_scores))
-student_info= sorted(student_info.items(),key=lambda x:x[1])
-x = np.arange(0,nstudents,1)
+if make_plots:
 
-print student_info
+    student_scores=sorted(student_scores)
+
+    average=sum(student_scores)/float(len(student_scores))
+    student_info= sorted(student_info.items(),key=lambda x:x[1])
+    num_of_students = np.arange(0,nstudents,1)
+
+    print student_info
 
 
-for i,student in enumerate(student_info):
-    plt.figure()
-    student_plot=plt.scatter(x,student_scores,color='b',marker='^',s=300,label='Individual student scores')
-    average_plot=plt.plot([0,nstudents],[average,average],'r--',label='Average score')
-    current_score = plt.scatter(i,student[1],color='g',marker='o',s=600,label='Your score')
-    plt.yticks(np.arange(0,100,5))
-    plt.xticks(np.arange(0,nstudents,1))
-    plt.xlim(-1,nstudents+1)
-    plt.legend(loc='lower left')
-    plt.title('Summary of Class Scores')
-    plt.xlabel('Student')
-    plt.ylabel('Score')
+    for i,student in enumerate(student_info):
+        plt.figure()
+        student_plot=plt.scatter(num_of_students,student_scores,color='b',marker='^',s=300,label='Individual student scores')
+        average_plot=plt.plot([0,nstudents],[average,average],'r--',label='Average score')
+        current_score = plt.scatter(i,student[1],color='g',marker='o',s=600,label='Your score')
+        plt.yticks(np.arange(0,100,5))
+        plt.xticks(np.arange(0,nstudents,1))
+        plt.xlim(-1,nstudents+1)
+        plt.legend(loc='lower left')
+        plt.title('Summary of Class Scores')
+        plt.xlabel('Student')
+        plt.ylabel('Score')
 
 ###############################################################################
 # Summarize the student responses and how the class performed on each
 # question and plot the summary of each student.
 ###############################################################################
-colors=['lightskyblue','lightcoral']
-labels=[r'Right',r'Wrong']
-question_label=[]
+    colors=['lightskyblue','lightcoral']
+    labels=[r'Right',r'Wrong']
+    question_label=[]
 
-x=0
-for q,question in zip(assignment_summary.transpose(),questions):
-    ntot = len(q)
-    ncorrect = q.sum()
-    print "Out of %d people %d got this question right---- %4.2f%%" % (ntot,ncorrect,100*ncorrect/ntot)
-    question_num = 'Question #' + str(x+1)
-    question_label.append(question_num)
-    the_grid=GridSpec(7,1)
-    sizes=[ncorrect,ntot-ncorrect]
-    plt.subplot(the_grid[x,0],aspect=1)
-    patches, texts = plt.pie(sizes,colors=colors,shadow=True)
-    plt.axis('equal')
-    plt.tight_layout()
-    x+=1
-    plt.title(question_num,fontweight='bold')
-    plt.text(1.5,0.1,'%4.1f%% of students got this question right' % (100*ncorrect/ntot))
-
-plt.legend(patches,labels,loc=('lower left'),shadow=True)
+    num_of_students=0
+    for q,question in zip(assignment_summary.transpose(),questions):
+        ntot = len(q)
+        ncorrect = q.sum()
+        print "Out of %d people %d got this question right---- %4.2f%%" % (ntot,ncorrect,100*ncorrect/ntot)
+        question_num = 'Question #' + str(num_of_students+1)
+        question_label.append(question_num)
+        the_grid=GridSpec(7,1)
+        sizes=[ncorrect,ntot-ncorrect]
+        plt.subplot(the_grid[num_of_students,0],aspect=1)
+        patches, texts = plt.pie(sizes,colors=colors,shadow=True)
+        plt.axis('equal')
+        plt.tight_layout()
+        num_of_students+=1
+        plt.title(question_num,fontweight='bold')
+        plt.text(1.5,0.1,'%4.1f%% of students got this question right' % (100*ncorrect/ntot))
+    plt.legend(patches,labels,loc=('lower left'),shadow=True)
     
 
-plt.show()
+    plt.show()
 
 
 
